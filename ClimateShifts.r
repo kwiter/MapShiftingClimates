0c22fecc780dff97951a5a767e7f96afacee45b7
kwiter <mckwit@gmail.com>
2015-06-22 01:02:46 -0400

    added polygons and clustering needs tweeking

--- ClimateShifts.r
+++ ClimateShifts.r
@@ -32,13 +32,14 @@ plot(fut70)
 
 all = stack(lgm,mid,cur,fut50,fut70)
 cliMat = cbind(coordinates(all),as.matrix(all))
-NCcliMat = cbind(coordinates(rr),as.matrix(rr))#clipped raster
+
 
 data.shape<-readOGR(dsn="/home/kwit/Dropbox/Shapes/cb_2014_us_state_500k",layer="cb_2014_us_state_500k")
 nc = data.shape[data.shape@data$NAME =='North Carolina',]
 nc <- spTransform(nc,crs(all))
 rr <- mask(all, nc)
 plot(rr)
+NCcliMat = cbind(coordinates(rr),as.matrix(rr))#clipped raster
 
 
 whr = apply(cliMat[,-c(1,2)],1,function(x) sum(!is.na(x)) != 0)
@@ -99,7 +100,7 @@ whrGMI = table(unlist(whrGMI))
 whrMHI = table(unlist(whrMHI))
 whr50I = table(unlist(whr50I))
 whr70I = table(unlist(whr70I))
-whrCUI = table(unlist(whrCUI))
+
 
 #all points
 #whrGM = unique(unlist(whrGM))
@@ -123,11 +124,72 @@ points(cliMat[c(whrCU),1],cliMat[c(whrCU),2],col='purple',pch=20,cex=.75)
 
 points(NCcliMat[,1],NCcliMat[,2],cex=.1)
 
-points(cliMat[c(a.n(names(whrGM))),1],cliMat[c(a.n(names(whrGM))),2],col=rgb(.2,.6,.6,whrGM/max(whrGM)),pch=20,cex=.4)
+points(cliMat[c(a.n(names(whrGM))),1],cliMat[c(a.n(names(whrGM))),2],col=groups,pch=20,cex=.4)
 points(cliMat[c(a.n(names(whrMH))),1],cliMat[c(a.n(names(whrMH))),2],col=rgb(.6,.6,.6,whrMH/max(whrMH)),pch=20,cex=.4)
-points(cliMat[c(a.n(names(whrCU))),1],cliMat[c(a.n(names(whrCU))),2],col=rgb(.2,.2,.6,whrCU/max(whrCU)),pch=20,cex=.4)
+points(cliMat[c(a.n(names(whrCU))),1],cliMat[c(a.n(names(whrCU))),2],col=groups,pch=20,cex=.4)
 points(cliMat[c(a.n(names(whr50))),1],cliMat[c(a.n(names(whr50))),2],col=rgb(.2,.2,.6,whr50/max(whr50)),pch=20,cex=.4)
 points(cliMat[c(a.n(names(whr70))),1],cliMat[c(a.n(names(whr70))),2],col=rgb(.2,.2,.6,whr70/max(whr70)),pch=20,cex=.4)
 
+points(cliMat[c(a.n(names(whrGMI))),1],cliMat[c(a.n(names(whrGMI))),2],col=rgb(.6,.6,.6,whrGMI/max(whrGMI)),pch=20,cex=.4)
+points(cliMat[c(a.n(names(whrMHI))),1],cliMat[c(a.n(names(whrMHI))),2],col=rgb(.6,.6,.6,whrMHI/max(whrMHI)),pch=20,cex=.4)
+points(cliMat[c(a.n(names(whrCU))),1],cliMat[c(a.n(names(whrCU))),2],col=rgb(.6,.6,.6,whrCU/max(whrCU)),pch=20,cex=.4)
+points(cliMat[c(a.n(names(whr50I))),1],cliMat[c(a.n(names(whr50I))),2],col=rgb(.2,.2,.6,whr50I/max(whr50I)),pch=20,cex=.4)
+points(cliMat[c(a.n(names(whr70I))),1],cliMat[c(a.n(names(whr70I))),2],col=rgb(.2,.2,.6,whr70I/max(whr70I)),pch=20,cex=.4)
 
 
+mydata = cliMat[a.n(names(whr70I)),c(1,2)]
+d <- dist(mydata, method = "euclidean") # distance matrix
+fit <- hclust(d, method="single")
+ng = 5
+groups <- cutree(fit, k=ng) # cut tree into k clusters
+
+par(mfrow= c(1,1),mar=c(2,2,2,1))
+plot(extent(all),xlab='',ylab='',bty='n',xaxt='n',yaxt='n')
+map(,add=T,col=1)
+map('state',add=T,col=1)
+for(j in 1:ng){
+if(length(a.n(whr70I[groups==j]))<10)next
+tmp = spline.poly(spatialQuantAbund(mydata[groups==j,],a.n(whr70I[groups==j]),nbrks = 10,quants = c(.2,.8)),50)
+polygon(tmp,lwd=3,lty=2,border=rgb(.4,.4,.8,1),col=rgb(.4,.4,.8,.5))
+}
+
+spatialQuantAbund = function(xyMat,abund,nbrks = 10,quants = c(.025,.975)){
+  stdX = sd(xyMat[,1],na.rm=T)
+  stdY = sd(xyMat[,2],na.rm=T)
+  
+  xyMat[,1] = (xyMat[,1]) /stdX
+  xyMat[,2] = (xyMat[,2]) /stdY
+  
+  mx = median(xyMat[,1])
+  my = median(xyMat[,2])
+  
+  tmp = angleTo(c(mx,my),xyMat)
+  angs = tmp$ang
+  tx = tmp$tx
+  ty = tmp$ty
+  quad = rep(0,len(tx))
+  quad[tx ==  1 & ty ==  1] = 1
+  quad[tx ==  1 & ty == -1] = 1
+  quad[tx == -1 & ty == -1] = -1
+  quad[tx == -1 & ty ==  1] = -1
+  
+  dis = distance(c(mx,my),xyMat)*abund
+  
+  
+  breaks <- seq(-90,90,length = nbrks)
+  bins <- cut(angs,breaks = breaks)
+  bins[which(is.na(bins))] = sort(unique(bins))[1]
+  iBin = sort(unique(cut(seq(-89,89,by = 1),breaks = breaks)))
+  tmp = matrix(NA,len(iBin),3)
+  tmp[,1] = breaks[-1] - diff(breaks)[1]/2
+  for(i in 1:len(iBin)){
+    whr = which(bins == iBin[i])
+    if(length(whr) == 0){tmp[i,c(2,3)] = c(0,0)
+    }else{
+    tmp[i,c(2,3)] = quantile(dis[whr] * quad[whr] / abund[whr],quants)
+    }
+  }
+  xs = (cos(c(tmp[,1],tmp[,1])*2*pi/360)*c(tmp[,2],tmp[,3]) + mx)*stdX 
+  ys = (sin(c(tmp[,1],tmp[,1])*2*pi/360)*c(tmp[,2],tmp[,3]) + my)*stdY 
+  cbind(xs,ys)
+}
